1ff29c4005016c9092c32259e22e181f4d2014f0
NightcrawlerEX <jsimp314159@gmail.com>
2026-06-13 19:09:40 +1000

    Query 3

--- Task 3 - Query 3.sql
+++ Task 3 - Query 3.sql
@@ -6,3 +6,11 @@
 -- Display Columns: First Name, Last Name, Gender and Faction Code.
 
 USE A00125081; -- database name is student number
+
+SELECT Player.FirstName, Player.LastName, Player.Gender, Faction.FactionCode
+FROM Player
+LEFT JOIN Member
+    ON Player.PlayerID = Member.PlayerID
+LEFT JOIN Faction
+    ON Faction.FactionID = Member.FactionID
+WHERE Faction.FactionCode = 'ROG'
