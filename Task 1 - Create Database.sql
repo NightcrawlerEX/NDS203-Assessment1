4874576b3dc33624a78af46795ccb863d8240677
NightcrawlerEX <jsimp314159@gmail.com>
2026-06-13 15:02:43 +1000

    Initial commit

---
+++ Task 1 - Create Database.sql
@@ -0,0 +1,15 @@
+-- Student: James Simpson
+-- Student No: A00125081
+-- Task 1: Design The Database
+
+-- Regions
+-- An argument could be made to have region type as its own table but we will cross that bridge when 
+-- we come to it. Most apps perform reads far more often than writes and joins are expensive so I
+-- won't create one just for expandability
+CREATE TABLE IF NOT EXISTS `mydb`.`Region` (
+  `RegionCode` VARCHAR(5) NOT NULL,
+  `RegionName` VARCHAR(45) NOT NULL,
+  `RegionType` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`RegionCode`),
+  UNIQUE INDEX `RegionCode_UNIQUE` (`RegionCode` ASC) VISIBLE)
+ENGINE = InnoDB;
---
+++ Task 2 - Insert Data.sql
@@ -0,0 +1,6 @@
+-- Student: James Simpson
+-- Student No: A00125081
+-- Task 2
+INSERT INTO Region (RegionCode, RegionName, RegionType) VALUES
+('ARC', 'Arcanum', 'Large Scale City'),
+('HER', 'Herodi' 'Magical Demiplane');
